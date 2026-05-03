fca0f2a6937638bb81bf1a2a7ba912fce6778ba3
alanmonterroso02-cmd <alanmonterroso02@gmail.com>
2026-05-03 01:55:49 -0600

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,386 +1,335 @@
 const participantes = [
-  //medicina
-    {
-      id: 1,
-      nombre: "Cabrera Lorenzo Esvin Santiago",
-      grado: "Quinto Medicina",
-    },
-    {
-      id: 2,
-      nombre: "Cortéz Hernández Jeferson José Luis",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 3,
-      nombre: "Cristóbal Pérez Lesly Beatriz",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 4,
-      nombre: "Delgado Vicente Noe Michael",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 5,
-      nombre: "Diego Morales Alexis Denilson",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 6,
-      nombre: "García Juárez María Del Carmen",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 7,
-      nombre: "Jiménez Cortéz Yeni Clarisa",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 8,
-      nombre: "Katherin Mishel Ralda Jiménez",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 9,
-      nombre: "Pérez Vaíl Keily Lisbeth",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 10,
-      nombre: "Ramírez Rodríguez Miqueas Emanuel",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 11,
-      nombre: "Reyes Gómez Jorge Geovany",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 12,
-      nombre: "Vásquez Lucas Fernado Ángel",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 13,
-      nombre: "Vásquez Luis Vivian Dayana",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 14,
-      nombre: "Velásquez Cabrera Mynor",
-      grado: "Quinto Medicina",
-      
-    },
-    {
-      id: 15,
-      nombre: "Jonatan Margarito",
-      grado: "Quinto Medicina",
-      
-    },
-    //enfermeria
-    {
-      id: 1,
-      nombre: "Alonzo Pérez Yaquelín Anayeli",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 2,
-      nombre: "Cabrera López Yuliza",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 3,
-      nombre: "Camacho López Glendy Lisbeth",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 4,
-      nombre: "Cortéz Ramos Yeni Carina",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 5,
-      nombre: "Díaz González Esmeralda Liseth",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 6,
-      nombre: "Hernández Díaz Sabina Lorenza",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 7,
-      nombre: "López Vásquez Keily Dayana",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 8,
-      nombre: "Ortega Pérez Leidy Noemí",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 9,
-      nombre: "Pérez Vásquez Vilma Daniela",
-      grado: "Quinto Enfermería",
-      
-    },
-    {
-      id: 10,
-      nombre: "Rabanales Gómez Neidy Fabiola",
-      grado: "Quinto Enfermería",
-      
-    },
-    //Derecho
-    {
-      id: 1,
-      nombre: "Marroquín Escobar Karla Andrea",
-      grado: "Quinto Derecho",
-      
-    },
-    {
-      id: 2,
-      nombre: "Monterroso Monterroso Beberly Belinda",
-      grado: "Quinto Derecho",
-      
-    },
-    {
-      id: 3,
-      nombre: "Reyes Monterroso Mariana Analy",
-      grado: "Quinto Derecho",
-      
-    },
-    {
-      id: 4,
-      nombre: "Vásquez Escobar Abraham Santos",
-      grado: "Quinto Derecho",
-      
-    },
-    {
-      id: 5,
-      nombre: "Vicente Chile Ashle Mariza",
-      grado: "Quinto Derecho",
-      
-    },
-    //Odontología
-    {
-      id: 1,
-      nombre: "Díaz Gómez José Mario",
-      grado: "Quinto Odontología",
-      
-    },
-    {
-      id: 2,
-      nombre: "Jimenéz Gómez Rolfi Hernan",
-      grado: "Quinto Odontología",
-      
-    },
-    {
-      id: 3,
-      nombre: "Méndez Méndez Anderson Hermenegildo",
-      grado: "Quinto Odontología",
-      
-    },
-    {
-      id: 4,
-      nombre: "Vásquez Gómez Melvin Gabriel",
-      grado: "Quinto Odontología",
-      
-    },
-    //Quinto Mecánica
-    {
-      id: 1,
-      nombre: "Alvarez Gantenbein Mario Estuardo",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 2,
-      nombre: "Calderón Rivera Edison Iván",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 3,
-      nombre: "Carreto Morales, Néstor Hernán",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 4,
-      nombre: "Delgado Vicente Elmer David",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 5,
-      nombre: "Escobar Méndez Isaac Esau",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 6,
-      nombre: "García Flores, Gilver Francisco Josué",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 7,
-      nombre: "López Romero Brayan Daniel",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 8,
-      nombre: "Marroquín Ramírez Wilfrido Daniel",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 9,
-      nombre: "Monterroso López Víctor Geovany",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 10,
-      nombre: "Orozco Vásquez Brayan Martín",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 11,
-      nombre: "Pérez Carreto Denilson Miguel",
-      grado: "Quinto Mecánica",
-      
-    },
-    {
-      id: 12,
-      nombre: "Romero López Iberton Alexis",
-      grado: "Quinto Mecánica",
-      
-    },
-    //Quinto Electromecánica
-    {
-      id: 1,
-      nombre: "Agustín López Oscar Rene",
-      grado: "Quinto Electromecánica",
-      
-    },
-    {
-      id: 2,
-      nombre: "Castillo Barrios Brandon Osiel",
-      grado: "Quinto Electromecánica",
-      
-    },
-    //musica
-    {
-      id: 1,
-      nombre: "López Romero Ester Estefany",
-      grado: "Quinto Música",
-      
-    },
-    //electrisidad
-    {
-      id: 1,
-      nombre: "López Gabriel Abner Gudiel",
-      grado: "Quinto Electricidad",
-      
-    },
-    {
-      id: 2,
-      nombre: "López Romero Andy Leonardo",
-      grado: "Quinto Electricidad",
-      
-    },
-    {
-      id: 3,
-      nombre: "Vicente Pérez Domingo Frisly",
-      grado: "Quinto Electricidad",
-      
-    },
-    {
-      id: 4,
-      nombre: "Vicente Vicente Axel Aroldo",
-      grado: "Quinto Electricidad",
-      
-    },
-    //diseño grfico
-    {
-      id: 1,
-      nombre: "Díaz Pérez, Werner Misael",
-      grado: "Quinto Diseño Gráfico",
-      
-    },
-    {
-      id: 2,
-      nombre: "Gómez Mejía Geraldine Zulimar",
-      grado: "Quinto Diseño Gráfico",
-      
-    },
-    {
-      id: 3,
-      nombre: "Pérez Romero Brailin Geovany",
-      grado: "Quinto Diseño Gráfico",
-      
-    },
-    {
-      id: 4,
-      nombre: "Monterroso Rodríguez Dulce Analucia",
-      grado: "Quinto Diseño Gráfico",
-      
-    },
-    //finansas
-    {
-      id: 1,
-      nombre: "De León Mazariegos Jennifer Anabelly",
-      grado: "Quinto Finanzas y Admo",
-      
-    },
-    {
-      id: 2,
-      nombre: "Sac Camacho Eddy Francisco",
-      grado: "Quinto Finanzas y Admo",
-      
-    },
-    {
-      id: 3,
-      nombre: "Sergio Gonsales",
-      grado: "Quinto Finanzas y Admo",
-      
-    },
-    {
-      id: 4,
-      nombre: "Werner Marokin Diaz",
-      grado: "Quinto Finanzas y Admo",
-      
-    },
-    //electronica
-      {
-      id: 1,
-      nombre: "Monterroso Gómez Alan Rene",
-      grado: "Quinto Electrónica",
-      
-    }
+// Medicina
+  {
+    id: 1,
+    nombre: "Cabrera Lorenzo Esvin Santiago",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 2,
+    nombre: "Cortéz Hernández Jeferson José Luis",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 3,
+    nombre: "Cristóbal Pérez Lesly Beatriz",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 4,
+    nombre: "Delgado Vicente Noe Michael",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 5,
+    nombre: "Diego Morales Alexis Denilson",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 6,
+    nombre: "García Juárez María Del Carmen",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 7,
+    nombre: "Jiménez Cortéz Yeni Clarisa",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 8,
+    nombre: "Katherin Mishel Ralda Jiménez",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 9,
+    nombre: "Pérez Vaíl Keily Lisbeth",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 10,
+    nombre: "Ramírez Rodríguez Miqueas Emanuel",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 11,
+    nombre: "Reyes Gómez Jorge Geovany",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 12,
+    nombre: "Vásquez Lucas Fernado Ángel",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 13,
+    nombre: "Vásquez Luis Vivian Dayana",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 14,
+    nombre: "Velásquez Cabrera Mynor",
+    grado: "Quinto Medicina"
+  },
+  {
+    id: 15,
+    nombre: "Jonatan Margarito",
+    grado: "Quinto Medicina"
+  },
+
+  // Enfermería
+  {
+    id: 16,
+    nombre: "Alonzo Pérez Yaquelín Anayeli",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 17,
+    nombre: "Cabrera López Yuliza",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 18,
+    nombre: "Camacho López Glendy Lisbeth",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 19,
+    nombre: "Cortéz Ramos Yeni Carina",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 20,
+    nombre: "Díaz González Esmeralda Liseth",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 21,
+    nombre: "Hernández Díaz Sabina Lorenza",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 22,
+    nombre: "López Vásquez Keily Dayana",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 23,
+    nombre: "Ortega Pérez Leidy Noemí",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 24,
+    nombre: "Pérez Vásquez Vilma Daniela",
+    grado: "Quinto Enfermería"
+  },
+  {
+    id: 25,
+    nombre: "Rabanales Gómez Neidy Fabiola",
+    grado: "Quinto Enfermería"
+  },
+
+  // Derecho
+  {
+    id: 26,
+    nombre: "Marroquín Escobar Karla Andrea",
+    grado: "Quinto Derecho"
+  },
+  {
+    id: 27,
+    nombre: "Monterroso Monterroso Beberly Belinda",
+    grado: "Quinto Derecho"
+  },
+  {
+    id: 28,
+    nombre: "Reyes Monterroso Mariana Analy",
+    grado: "Quinto Derecho"
+  },
+  {
+    id: 29,
+    nombre: "Vásquez Escobar Abraham Santos",
+    grado: "Quinto Derecho"
+  },
+  {
+    id: 30,
+    nombre: "Vicente Chile Ashle Mariza",
+    grado: "Quinto Derecho"
+  },
+
+  // Odontología
+  {
+    id: 31,
+    nombre: "Díaz Gómez José Mario",
+    grado: "Quinto Odontología"
+  },
+  {
+    id: 32,
+    nombre: "Jimenéz Gómez Rolfi Hernan",
+    grado: "Quinto Odontología"
+  },
+  {
+    id: 33,
+    nombre: "Méndez Méndez Anderson Hermenegildo",
+    grado: "Quinto Odontología"
+  },
+  {
+    id: 34,
+    nombre: "Vásquez Gómez Melvin Gabriel",
+    grado: "Quinto Odontología"
+  },
+
+  // Mecánica
+  {
+    id: 35,
+    nombre: "Alvarez Gantenbein Mario Estuardo",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 36,
+    nombre: "Calderón Rivera Edison Iván",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 37,
+    nombre: "Carreto Morales Néstor Hernán",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 38,
+    nombre: "Delgado Vicente Elmer David",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 39,
+    nombre: "Escobar Méndez Isaac Esau",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 40,
+    nombre: "García Flores Gilver Francisco Josué",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 41,
+    nombre: "López Romero Brayan Daniel",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 42,
+    nombre: "Marroquín Ramírez Wilfrido Daniel",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 43,
+    nombre: "Monterroso López Víctor Geovany",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 44,
+    nombre: "Orozco Vásquez Brayan Martín",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 45,
+    nombre: "Pérez Carreto Denilson Miguel",
+    grado: "Quinto Mecánica"
+  },
+  {
+    id: 46,
+    nombre: "Romero López Iberton Alexis",
+    grado: "Quinto Mecánica"
+  },
+
+  // Electromecánica
+  {
+    id: 47,
+    nombre: "Agustín López Oscar Rene",
+    grado: "Quinto Electromecánica"
+  },
+  {
+    id: 48,
+    nombre: "Castillo Barrios Brandon Osiel",
+    grado: "Quinto Electromecánica"
+  },
+
+  // Música
+  {
+    id: 49,
+    nombre: "López Romero Ester Estefany",
+    grado: "Quinto Música"
+  },
+
+  // Electricidad
+  {
+    id: 50,
+    nombre: "López Gabriel Abner Gudiel",
+    grado: "Quinto Electricidad"
+  },
+  {
+    id: 51,
+    nombre: "López Romero Andy Leonardo",
+    grado: "Quinto Electricidad"
+  },
+  {
+    id: 52,
+    nombre: "Vicente Pérez Domingo Frisly",
+    grado: "Quinto Electricidad"
+  },
+  {
+    id: 53,
+    nombre: "Vicente Vicente Axel Aroldo",
+    grado: "Quinto Electricidad"
+  },
+
+  // Diseño Gráfico
+  {
+    id: 54,
+    nombre: "Díaz Pérez Werner Misael",
+    grado: "Quinto Diseño Gráfico"
+  },
+  {
+    id: 55,
+    nombre: "Gómez Mejía Geraldine Zulimar",
+    grado: "Quinto Diseño Gráfico"
+  },
+  {
+    id: 56,
+    nombre: "Pérez Romero Brailin Geovany",
+    grado: "Quinto Diseño Gráfico"
+  },
+  {
+    id: 57,
+    nombre: "Monterroso Rodríguez Dulce Analucia",
+    grado: "Quinto Diseño Gráfico"
+  },
+
+  // Finanzas
+  {
+    id: 58,
+    nombre: "De León Mazariegos Jennifer Anabelly",
+    grado: "Quinto Finanzas y Admo"
+  },
+  {
+    id: 59,
+    nombre: "Sac Camacho Eddy Francisco",
+    grado: "Quinto Finanzas y Admo"
+  },
+  {
+    id: 60,
+    nombre: "Sergio Gonsales",
+    grado: "Quinto Finanzas y Admo"
+  },
+  {
+    id: 61,
+    nombre: "Werner Marokin Diaz",
+    grado: "Quinto Finanzas y Admo"
+  },
+
+  // Electrónica
+  {
+    id: 62,
+    nombre: "Monterroso Gómez Alan Rene",
+    grado: "Quinto Electrónica"
+  }
 ];
 
 const contenedor = document.getElementById("qrs");
